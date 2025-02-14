35f561d25747f22c0d99468b620bba1954310d28
Budilenko Olexander <sahok123444@gmail.com>
2025-02-14 21:53:40 +0200

    endpoint 2

---
+++ .history/src/components/PropertiesPage/FiltersForProperties/FiltersForProperties_20250214162255.jsx
@@ -0,0 +1,106 @@
+import React, { useState, useEffect, useRef } from "react";
+import locationImg from "../../../images/icons/filters/location.svg";
+import propertyTypeImg from "../../../images/icons/filters/propertyType.svg";
+import pricingRangeImg from "../../../images/icons/filters/pricingRange.svg";
+buildYearImg from "../../../images/icons/filters/buildYear.svg";
+import "./properitesFilters.scss";
+
+const FiltersForProperties = ({ onFiltersChange }) => {
+  const [activeFilter, setActiveFilter] = useState(null);
+  const [filterInputs, setFilterInputs] = useState({});
+  const [filterData, setFilterData] = useState([]);
+  const dropdownRefs = useRef({});
+
+  useEffect(() => {
+    const fetchFilters = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/filters");
+        const data = await response.json();
+        setFilterData(data);
+
+        // Инициализируем фильтры
+        const initialInputs = {};
+        data.forEach((filter) => {
+          initialInputs[filter.name] = "";
+        });
+        setFilterInputs(initialInputs);
+      } catch (err) {
+        console.error("Ошибка загрузки фильтров:", err);
+      }
+    };
+
+    fetchFilters();
+  }, []);
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (
+        activeFilter &&
+        dropdownRefs.current[activeFilter] &&
+        !dropdownRefs.current[activeFilter].contains(event.target)
+      ) {
+        setActiveFilter(null);
+      }
+    };
+
+    document.addEventListener("mousedown", handleOutsideClick);
+    return () => document.removeEventListener("mousedown", handleOutsideClick);
+  }, [activeFilter]);
+
+  const toggleFilter = (filterName) => {
+    setActiveFilter(activeFilter === filterName ? null : filterName);
+  };
+
+  const selectOption = (filterName, option) => {
+    setFilterInputs((prev) => ({
+      ...prev,
+      [filterName]: option,
+    }));
+    setActiveFilter(null);
+
+    onFiltersChange({ ...filterInputs, [filterName]: option });
+  };
+
+  return (
+    <div className="filters-for__properties">
+      {filterData.map((filter, index) => (
+        <div
+          className="filter__item"
+          key={index}
+          ref={(el) => (dropdownRefs.current[filter.name] = el)}
+        >
+          <img
+            src={
+              filter.name === "location"
+                ? locationImg
+                : filter.name === "type"
+                ? propertyTypeImg
+                : filter.name === "price"
+                ? pricingRangeImg
+                : buildYearImg
+            }
+            alt=""
+          />
+          <input
+            type="text"
+            placeholder={filter.name}
+            value={filterInputs[filter.name]}
+            onClick={() => toggleFilter(filter.name)}
+            readOnly
+          />
+          {activeFilter === filter.name && (
+            <ul className="dropdown">
+              {filter.options.map((option, i) => (
+                <li key={i} onClick={() => selectOption(filter.name, option + (filter.name === "price" ? "$+" : filter.name === "propertySize" ? " sqm+" : ""))}>
+                  {option}{filter.name === "price" ? "$+" : filter.name === "propertySize" ? " sqm+" : ""}
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default FiltersForProperties;
